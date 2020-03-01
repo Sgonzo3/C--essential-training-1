b3db0256ab257a1e2a53b4f5952dbf0975b82754
Santos Gonzalez <sgonzo3@gmail.com>
2020-02-29 22:41:30 -0800

    feat: HelloWorld and comments challenges complete

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// An example XML comment
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // A single line comment
+
+            /*
+             * First line in multiline comment
+             * Second line in multi line comment
+            */
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
